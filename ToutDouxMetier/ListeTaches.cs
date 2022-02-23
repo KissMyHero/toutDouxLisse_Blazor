accc516df852516ea2c5030ca9900940c2198328
Christophe Lorenzato <christophe@rhaegal.com>
2022-02-23 16:35:08 +0100

    crud tache

---
+++ ToutDouxMetier/ListeTaches.cs
@@ -0,0 +1,33 @@
+namespace ToutDouxMetier;
+
+public class ListeTaches
+{
+    private int id = 0;
+    private Dictionary<int, Tache> listeTaches = new Dictionary<int, Tache>();
+
+    public ListeTaches(params string [] intituleTaches)
+    {
+        foreach(string intitule in intituleTaches)
+        {
+            Add(intitule);
+        }
+    }
+    public void Add(string intitule)
+    {
+        var tache = new Tache(++id, intitule);
+
+        listeTaches.Add(tache.Id, tache);
+    }
+
+    public void TriggerDone(int id)
+    {
+        listeTaches[id].Fait = !listeTaches[id].Fait;
+    }
+
+    public void Remove(int id)
+    {
+        listeTaches.Remove(id);
+    }
+
+    public IEnumerable<Tache> Taches => listeTaches.Values;
+}
---
+++ ToutDouxMetier/Tache.cs
@@ -0,0 +1,12 @@
+﻿namespace ToutDouxMetier;
+public class Tache
+{
+    public Tache(int id, string intitule)
+    {
+        Id = id;
+        Intitule = intitule;
+    }
+    public int Id { get; private init; }
+    public bool Fait { get; set; }
+    public string Intitule { get; set; }
+}
